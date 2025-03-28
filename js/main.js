7c43a8da3e6323633c25f8047d2c00c677953078
babinicz <babinicz@onet.pl>
2025-03-28 14:30:49 +0100

    zm

--- js/main.js
+++ js/main.js
@@ -29,28 +29,3 @@ $(document).ready(function () {
     once: true,
   });
 });
-
-window.fbAsyncInit = function () {
-  FB.init({
-    xfbml: true,
-    version: "v6.0",
-  });
-
-  // Kod do ustawienia 'page_id' w odpowiednim miejscu
-  FB.CustomerChat.init({
-    page_id: "100945721456541",
-    theme_color: "#0084ff",
-    logged_in_greeting: "Hello!",
-    logged_out_greeting: "Goodbye!",
-  });
-};
-
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
