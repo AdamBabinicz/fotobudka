cf833517b2d61b9f17a901cced9cb2750c07e2e6
babinicz <babinicz@onet.pl>
2025-03-28 13:14:54 +0100

    zmiany

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-  $(".menu-toggler").on("click", function() {
+$(document).ready(function () {
+  $(".menu-toggler").on("click", function () {
     $(this).toggleClass("open");
     $(".top-nav").toggleClass("open");
   });
-  $(".top-nav .nav-link").on("click", function() {
+  $(".top-nav .nav-link").on("click", function () {
     $(".menu-toggler").removeClass("open");
     $(".top-nav").removeClass("open");
   });
-  $('nav a[href*="#"]').on("click", function() {
+  $('nav a[href*="#"]').on("click", function () {
     $("html, body").animate(
       {
-        scrollTop: $($(this).attr("href")).offset().top - 100
+        scrollTop: $($(this).attr("href")).offset().top - 100,
       },
       2000
     );
   });
-  $("#up").on("click", function() {
+  $("#up").on("click", function () {
     $("html, body").animate(
       {
-        scrollTop: 0
+        scrollTop: 0,
       },
       2000
     );
@@ -26,6 +26,31 @@ $(document).ready(function() {
   AOS.init({
     easing: "ease",
     duration: 1800,
-    once: true
+    once: true,
   });
 });
+
+window.fbAsyncInit = function () {
+  FB.init({
+    xfbml: true,
+    version: "v6.0",
+  });
+
+  // Kod do ustawienia 'page_id' w odpowiednim miejscu
+  FB.CustomerChat.init({
+    page_id: "100945721456541",
+    theme_color: "#0084ff",
+    logged_in_greeting: "Hello!",
+    logged_out_greeting: "Goodbye!",
+  });
+};
+
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
